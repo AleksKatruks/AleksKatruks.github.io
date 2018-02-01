2dc80a93ad62c3c9a0bef1be9960f3f646e2edc8
AleksKatruks <katruksasha@gmail.com>
2018-02-01 23:43:21 +0200

    assdsss

--- js/popup.js
+++ js/popup.js
@@ -3,37 +3,37 @@ $(function(){
 	var model = {
 		nashe: {
 		  title:  'Nashe online store',
-          detail: 'It was my first website where I used AngularJS. There was used routing and ajax request' +
-                  'Also I made simple design with making accent on product'+
-                  'It had to be simple in use without bucket, just contact form.'		  		  
+          detail: 'It was my first website, where I used AngularJS. There were used routing and ajax request' +
+                  'Also I made a simple design with focussing on a product'+
+                  'It had to be simple in use without bucket, just through the contact form.'		  		  
 				  
 	    },
 		betrue: {
 		  title:  'Betrue brand',
-          detail: 'It was the first full completed project where I have designed and developed(HTML/CSS/JS) a website - oline store for "Betrue" brand (clothes selling)' +
-                  'The design of the website was created in a minimalistic style, using Photoshop' +
-				  'There was established a simple bucket. When you add to bucket positions data is stored in localstorage.'	,
+          detail: 'It was my  first fully completed project, where I designed and developed(HTML/CSS/JS) a website - online store for "Betrue" brand (clothes selling)' +
+                  'The design of the website was created in minimalistic style with the use of Photoshop' +
+				  'There was established a simple bucket. When you add positions to the bucket, data is stored in the localstorage.'	,
           linkto: 'www.betrue.com.ua'				  
 		},
 		dispatch: {
 		  title:  'E-mail Dispatch',
-          detail: '"Dispatch email" - Email marketing campaigns. In the framework of the project I was coordinating I had to to design templates( HTML table markup) for sending them to doctors. I used Photoshop and Illustrator while creating a design.' +
+          detail: '"E-mail Dispatch" - Email marketing campaigns. In the framework of the project I was coordinating different teams. We had to design templates( HTML table markup), that further were sent to doctors. I used Photoshop and Illustrator while creating a design.' +
                   'I have launched more than 8 designes'
 		},
 		videoplugin: {
 		  title:  'Pfizers videopage',
-          detail: 'Video page - a task was to create a page, where all videos of each product were gathered togather in groups. But since the website was hosted on Drupal CMS,' +
-                  'I had limited possibilities in creating and development. So there were acceptable only pure JS.'+
-				  'I created simple UI with function of video grouping/filter by the name of a particular product.',
+          detail: 'Video page - a task was to create a page, where all videos of each product were gathered together in groups. But since the website was hosted on Drupal CMS,' +
+                  'I had limited possibilities in creating and development. So, I used only pure JS.'+
+				  'I created a simple UI with a function of video grouping/filter by the name of a particular product.',
            linkto: 'www.pfizermed.com.ua/uk-UA/node/1246?destination=uk-UA/node/1246'				  
 		},
 		a2o: {
 		  title:  'A20 - furniture brand',
-          detail: 'Here I created simple design of landing page. The main aim is to share information about company. I used yellow and black colors and block structure'            	  		     			
+          detail: 'Here I created a simple design of a landing page. The main aim is to share information about the company. I used yellow and black colors, and block structure'            	  		     			
 		},
 		travel: {
 		  title:  'Selected travel',
-          detail: 'Main idea of simple site to select appropriate direction for you. You need to answer on short survey and site define you wishis which you would like to get from travel', 
+          detail: 'The main idea of the site is to select an appropriate journey destination. You need to answer a short survey and the site will define, which place will meet your wishes and be the best for your visit', 
           linkto: 'www.behance.net/gallery/58795577/Carpathian-website-for-travelers'		  
 		}
 	};
